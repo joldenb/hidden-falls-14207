bd6e0320c475193288984b7439af56bf1aeb8ea0
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-02-01 10:15:33 -0700

    adding bootstrap and passport

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var path = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+exports.index = function(req, res){
+  res.render('index', { title: "Start Bootstrap"});
+};
 
-module.exports = router;
+exports.ping = function(req, res){
+  res.send("pong!", 200);
+};
